8cefc567a892818cc5cf80c84b7453d1925c7410
bartoszpro <bartoszprotasewicz@gmail.com>
2022-02-06 23:11:22 -0500

    add game -- header

---
+++ text based/game.cpp
@@ -0,0 +1,8 @@
+//
+//  game.cpp
+//  text based
+//
+//  Created by Bartosz Protasewicz on 2/6/22.
+//
+
+#include "game.hpp"
---
+++ text based/game.hpp
@@ -0,0 +1,13 @@
+//
+//  game.hpp
+//  text based
+//
+//  Created by Bartosz Protasewicz on 2/6/22.
+//
+
+#ifndef game_hpp
+#define game_hpp
+
+#include <stdio.h>
+
+#endif /* game_hpp */
