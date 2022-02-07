0ffafcaf625709a0c279f80edb6364e326a3626c
bartoszpro <bartoszprotasewicz@gmail.com>
2022-02-06 22:42:59 -0500

    title image added

--- text based/main.cpp
+++ text based/main.cpp
@@ -6,9 +6,31 @@
 //
 
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string titleImage = "";
+    string fileName = "title.rtf";
+    
+    ifstream inputFile;
+    inputFile.open(fileName.c_str());
+    
+    if (inputFile)
+    {
+        while (inputFile)
+        {
+            string temp;
+            getline(inputFile, temp);
+            temp += "\n";
+            titleImage += temp;
+        }
+        cout << titleImage << endl;
+        
+        inputFile.close();
+    }
+    
+    
     return 0;
 }
